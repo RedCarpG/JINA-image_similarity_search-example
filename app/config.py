6a5f96440715c3f3e333fc615573cf69c5a0fda9
NatakuG <512828239@qq.com>
2021-10-29 11:41:16 +0200

    feat: add main.py and app

---
+++ app/config.py
@@ -0,0 +1,19 @@
+import os
+
+MAX_DOCS = int(os.environ.get("JINA_MAX_DOCS", 10000))
+
+DEFAULT_QUERY_IMAGE = 'data/example.jpg'
+DEFAULT_QUERY_TEXT = 'example'
+
+BACKEND_PORT = 12345
+WORKSPACE_DIR = 'workspace'
+WORKSPACE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../"+ WORKSPACE_DIR)
+LOG_LEVEL = 'INFO'
+
+def config_env():
+    workspace_mount = f'{os.environ.get("JINA_WORKSPACE")}:/workspace/workspace'
+
+    os.environ.setdefault('JINA_WORKSPACE', WORKSPACE_PATH)
+    os.environ.setdefault('JINA_WORKSPACE_MOUNT', workspace_mount)
+    os.environ.setdefault('JINA_LOG_LEVEL', LOG_LEVEL)
+    os.environ.setdefault('JINA_PORT', str(BACKEND_PORT))
